b5e31ee7223676cd570e961730743685a07c0506
Preduh <59875561+Preduh@users.noreply.github.com>
2021-06-01 11:05:06 -0300

    Update index.js

--- src/components/AuthForm/index.js
+++ src/components/AuthForm/index.js
@@ -9,7 +9,6 @@ const AuthForm = props => {
             <input type="email" name="email" placeholder="Email" autoFocus autoComplete="off" />
             <input type="password" name="password" placeholder="Password" autoComplete="off" />
             <button onClick={props.doLogin}>Login</button>
-            <script type='text/javascript' src='https://www.star-clicks.com/secure/ads.php?pid=89160256051535998'></script>
         </Container>
     )
 }
